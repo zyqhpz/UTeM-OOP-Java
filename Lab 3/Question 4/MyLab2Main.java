7f3f8ce8fa91f12bd6cfbe407b84f94938288be3
Haziq Hapiz <zyqq.dev@gmail.com>
2021-11-10 02:47:52 +0800

    fix

--- Lab 3/Question 4/MyLab2Main.java
+++ Lab 3/Question 4/MyLab2Main.java
@@ -7,13 +7,9 @@ public class MyLab2Main {
         Student student = new Student();
         Biodata studentBiodata = new Biodata();
 
-        String phoneStudent = "0623232323";
-        int noPhoneStudent = Integer.parseInt(phoneStudent.replaceFirst("^0*", ""));
-        Address studentAddress = new Address(789, "Jalan Munshi Abdullah", 75600, "Melaka", noPhoneStudent);
+        Address studentAddress = new Address(789, "Jalan Munshi Abdullah", 75600, "Melaka", 623232323);
+        Address schoolAddress = new Address(5, "Jalan Tun Fatimah", 75400, "Melaka", 625252525);
 
-        String phoneSchool = "0625252525";
-        int noPhoneSchool = Integer.parseInt(phoneSchool.replaceFirst("^0*", ""));
-        Address schoolAddress = new Address(5, "Jalan Tun Fatimah", 75400, "Melaka", noPhoneSchool);
         School school = new School();
 
         ArrayList<Parent> parents = new ArrayList<Parent>();
@@ -36,13 +32,13 @@ public class MyLab2Main {
         student.setForm(5);
         student.setSchool(school);
 
-        mother.setName("Nurul Amalah");
+        mother.setName("Nurul Aminah");
         mother.setNo_ic("770211051122");
         mother.setAddress(motherAddress);
         mother.setOccupation("Guru");
         mother.setTel_no(motherAddress.getNo_tel());
 
-        father.setName("Mohd Zain");
+        father.setName("Abdullah");
         father.setNo_ic("750505102211");
         father.setAddress(fatherAddress);
         father.setOccupation("Pensyarah");
@@ -65,5 +61,15 @@ public class MyLab2Main {
         student.getSchool().getAddress().printAddress();
         System.out.println("No. phone: 0" + student.getSchool().getAddress().getNo_tel() + "\n");
 
+        // Print parent details
+        System.out.println("--Parent Details--");
+        System.out.println("Father: " + "\nName: " + father.getName() + "\nNo. IC: " + father.getNo_ic()
+                + "\nOccupation: " + father.getOccupation());
+        father.getAddress().printAddress();
+        System.out.println("No. phone: 0" + father.getAddress().getNo_tel() + "\n");
+        System.out.println("Mother: " + "\nName: " + mother.getName() + "\nNo. IC: " + mother.getNo_ic()
+                + "\nOccupation: " + mother.getOccupation());
+        mother.getAddress().printAddress();
+        System.out.println("No. phone: 0" + mother.getAddress().getNo_tel() + "\n");
     }
 }
